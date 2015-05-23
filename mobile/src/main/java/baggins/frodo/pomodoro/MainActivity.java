58fde9733fe7440a6a61870c348db9568f7df572
zsogolow <zachsogolow@msn.com>
2015-05-22 22:15:55 -0700

    Added an AnalogClock and TextClock.

--- mobile/src/main/java/baggins/frodo/pomodoro/MainActivity.java
+++ mobile/src/main/java/baggins/frodo/pomodoro/MainActivity.java
@@ -1,17 +1,50 @@
 package baggins.frodo.pomodoro;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AnalogClock;
+import android.widget.TextClock;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements DialogInterface.OnClickListener{
+
+    AnalogClock analog = null;
+    TextClock textClock = null;
+
+    Handler timerHandler = new Handler();
+    Runnable timerRunnable = new Runnable() {
+    long startTime = 0;
+        @Override
+        public void run() {
+            long millis = System.currentTimeMillis() - startTime;
+            int seconds = (int) (millis / 1000);
+            int minutes = seconds / 60;
+            seconds = seconds % 60;
+            Log.d("Tag", ""+seconds);
+
+            analog.postInvalidate();
+            textClock.postInvalidate();
+
+            timerHandler.postDelayed(this, 500);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        analog = (AnalogClock) findViewById(R.id.clock);
+        textClock = (TextClock) findViewById(R.id.textclock);
+
+
+
+        timerRunnable.run();
     }
 
     @Override
@@ -34,5 +67,10 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(DialogInterface dialog, int which) {
+
     }
 }
