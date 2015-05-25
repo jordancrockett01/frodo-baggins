9601864badc8d22b193ce38e82389e575d1eb6f1
zsogolow <zachsogolow@msn.com>
2015-05-25 11:12:45 -0700

    Added a background service and a notification manager that are not currently working.

--- mobile/src/main/java/baggins/frodo/pomodoro/AlarmService.java
+++ mobile/src/main/java/baggins/frodo/pomodoro/AlarmService.java
@@ -1,13 +1,10 @@
 package baggins.frodo.pomodoro;
 
 import android.app.IntentService;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Handler;
 import android.os.IBinder;
-import android.support.v4.app.NotificationCompat;
 import android.support.v4.content.LocalBroadcastManager;
 
 /**
